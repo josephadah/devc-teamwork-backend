05e5f1e9a5fe57faf6a046416331608edb697b22
Joseph Adah Jonathan <jonathanadahjoe@gmail.com>
2019-11-11 20:35:16 +0100

    add eslint and postgresql pg package

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-const express = require("express");
-const config = require("config");
+const express = require('express');
+const config = require('config');
+
 const app = express();
 
-require("./startup/routes")(app);
+require('./startup/routes')(app);
 
-const port = process.env.PORT || config.get("port");
+const port = process.env.PORT || config.get('port');
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = server;
