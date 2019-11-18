02bafda7a444453256d2d87d55157cc5729d6ac2
Joseph Adah Jonathan <jonathanadahjoe@gmail.com>
2019-11-18 06:01:23 +0100

    setting db config

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-const express = require('express');
-const config = require('config');
+const express = require("express");
+const { Pool } = require("pg");
+const config = require("config");
 
 const app = express();
 
-require('./startup/routes')(app);
+require("./startup/routes")(app);
 
-const port = process.env.PORT || config.get('port');
+const port = process.env.PORT || config.get("port");
+
+// const pool = new Pool({
+//   user: "josephadah",
+//   host: "localhost",
+//   database: "testing",
+//   password: "password",
+//   port: "5432"
+// });
+
+// pool.query("SELECT NOW()", (err, res) => {
+//   console.log(err, res);
+//   pool.end();
+// });
+
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
 
 module.exports = server;
