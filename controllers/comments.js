e58b7ab51b79bc82805fb25be97425fe0910ed00
developer3 <jonathanadahjoe@gmail.com>
2019-11-25 18:36:30 +0100

    wip adding comments endpoints

---
+++ controllers/comments.js
@@ -0,0 +1,128 @@
+const { pool } = require("../config/db");
+
+const getAllArticles = (req, res) => {
+  try {
+    pool.query(`SELECT * FROM comments ORDER BY "createdOn" DESC`, (error, results) => {
+      if (error) {
+        throw error;
+      }
+      res.status(200).json({ status: "success", data: results.rows });
+    });
+  } catch (error) {
+    throw error;
+  }
+};
+
+const getArticle = (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    pool.query(`SELECT * FROM comments WHERE "articleId" = $1`, [id],
+      (error, results) => {
+        if (error) throw error;
+        res.status(200).json({ status: "success", data: results.rows });
+      }
+    );
+  } catch (err) {
+    throw err;
+  }
+};
+
+const postArticle = (req, res) => {
+  try {
+    const { title, article } = req.body;
+    pool.query(
+      `INSERT INTO comments (title, article, "userId") VALUES ($1, $2, $3) RETURNING *`,
+      [title, article, req.user.userId],
+      (error, result) => {
+        if (error) {
+          throw error;
+        }
+        const { articleId, createdOn } = result.rows[0];
+        res.status(201).json({
+          status: "success",
+          data: {
+            message: "Article successfully posted",
+            articleId,
+            createdOn,
+            title
+          }
+        });
+      }
+    );
+  } catch (err) {
+    throw err;
+  }
+};
+
+const editArticle = (req, res) => {
+  try {
+    const { title, article } = req.body;
+    const id = parseInt(req.params.id, 10);
+
+    pool.query(`SELECT * FROM comments WHERE "articleId" = $1`, [id],
+      (error, results) => {
+        if (error) throw error;
+        const dbArticle =results.rows[0];
+        if (dbArticle.userId !== req.user.userId) return res.status(400).json({"status":"error", "error": "operation denied"});
+        
+        pool.query(
+          `UPDATE comments SET title = $1, article = $2 WHERE "articleId" = $3`,
+          [title, article, id],
+          (error, result) => {
+            if (error) throw error;
+            res.status(200).json({
+              status: "success",
+              data: {
+                message: "Article successfully updated",
+                title,
+                article
+              }
+            });
+          }
+        );
+      }
+    );
+
+  } catch (err) {
+    throw err;
+  }
+};
+
+const deleteArticle = (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10);
+    
+    pool.query(`SELECT * FROM comments WHERE "articleId" = $1`, [id], (error, results) => {
+      if (error) throw error;
+      const article = results.rows[0];
+      if (article) {
+        if (article.userId !== req.user.userId) return res.status(400).json({"status":"error", "error": "operation denied"});
+        
+        pool.query(`DELETE FROM comments WHERE "articleId" = $1`, [id], (error, result) => {
+            if (error) throw error;
+            res.status(200).json({
+              status: "success",
+              data: {
+                message: "Article successfully deleted"
+              }
+            });
+          }
+        );
+      } else {
+        res.status(404).json({ status: "error", "error": "article not found" });
+      }
+    }
+  );
+
+  } catch (err) {
+    throw err;
+  }
+};
+
+module.exports = {
+  getAllArticles,
+  getArticle,
+  postArticle,
+  editArticle,
+  deleteArticle
+};
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 var bodyParser = require('body-parser');
 const config = require("config");
+const cors = require('cors');
 const dotenv = require("dotenv");
 const { resolve } = require("path");
+const { pool } = require("./config/db");
 
 const app = express();
 
@@ -11,8 +13,41 @@ app.use(express.static(resolve(__dirname, 'src/public')));
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
+app.use(cors());
 
 require("./startup/routes")(app);
+
+
+
+// just for test
+const getBooks = (request, response) => {
+    pool.query('SELECT * FROM books', (error, results) => {
+      if (error) {
+        throw error
+      }
+      response.status(200).json(results.rows)
+    })
+  }
+  
+  const addBook = (request, response) => {
+    const { author, title } = request.body
+  
+    pool.query('INSERT INTO books (author, title) VALUES ($1, $2)', [author, title], error => {
+      if (error) {
+        throw error
+      }
+      response.status(201).json({ status: 'success', message: 'Book added.' })
+    })
+  }
+  
+  app
+    .route('/books')
+    // GET endpoint
+    .get(getBooks)
+    // POST endpoint
+    .post(addBook)
+
+
 
 const port = process.env.PORT || config.get("port");
 
